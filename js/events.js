6d4e56903696f143e51879a662898601f99ee8f2
Nick Spangler <spangleyman@mac.com>
2020-05-26 20:19:10 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt(){
 }
 
 function submitIt() {
-  $("form").on("submit", function() {
+  $("form").submit(function() {
     alert("Your form is going to be submnitted now.")
   })
 }
